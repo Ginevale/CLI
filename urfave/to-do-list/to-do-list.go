b56394bf988e6e2fa1e6eb142f233bb7d6fe42f4
Ginevra <ginevra.valerio@steamschoolemilia.it>
2023-06-30 12:53:14 +0200

    feat(added-functions): implement of coci and costi test and curiosity with the students' names

--- urfave/to-do-list/to-do-list.go
+++ urfave/to-do-list/to-do-list.go
@@ -15,8 +15,135 @@ func assessment() {
 	fmt.Println("Today", student, "will be assessed")
 }
 
+func cociTest(num int) {
+	switch {
+	case num == 1:
+		var ans int
+		fmt.Println("How many bits are contained in a byte?")
+		fmt.Scanln(&ans)
+		ans = int(ans)
+
+		if ans == 8 {
+			fmt.Println("In a byte there are 8 bits ")
+		} else {
+			fmt.Println("In a byte there aren't", ans, "bits. Try again!")
+		}
+	case num == 2:
+		fmt.Println("L = []")
+		fmt.Println("L = L.append(4)")
+		fmt.Println("print (L)")
+		var ans string
+		fmt.Println("What does this program print?")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+
+		if ans == "4" {
+			fmt.Println("Good job!")
+		} else {
+			fmt.Println(ans, "isn't correct. Try again!")
+		}
+	case num == 3:
+		var ans string
+		fmt.Println("Write a loop type")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+
+		if ans == "for" || ans == "while" {
+			fmt.Println("That's right!")
+		} else {
+			fmt.Println(ans, "isn't correct. Try again!")
+		}
+	case num == 4:
+		var ans string
+		fmt.Println("What sequence can be modified? A)String B)List C)Tuple")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+
+		if ans == "B" || ans == "b" {
+			fmt.Println("That's right!")
+		} else {
+			fmt.Println(ans, "isn't correct. Try again!")
+		}
+	}
+}
+
+func cociCuriosity(num int) {
+	switch {
+	case num == 1:
+		fmt.Println("The byte is a unit of digital information that consists of eight bits. ")
+	case num == 2:
+		fmt.Println("To Insert an element at the bottom of a list: L.append (x) or to insert an element in a specific position i: L.insert (i, x)")
+	case num == 3:
+		fmt.Println("A while loop repeats as long as the condition holds true, while a for loop is used to loop through an iterable object (like a list, tuple, set, etc.) and perform the same action for each entry.")
+	case num == 4:
+		fmt.Println("Strings and Tuples are immutabile, while lists are mutable. Their values can be changed.")
+	}
+}
+
+func costiTest(num int) {
+	switch {
+	case num == 1:
+		var ans string
+		fmt.Println("Who wrote Dorian Gray?")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+		if ans == "Oscar Wilde" {
+			fmt.Println("Dorian Gray was written by Oscar Wilde")
+		} else {
+			fmt.Println("Dorian Gray wasn't written by ", ans, "Try again!")
+		}
+	case num == 2:
+		var ans string
+		fmt.Println("What's Gulliver's role on the land of Lilliput? A)To satire on British scientific community B)To criticises humanity C)Benevolent giant")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+
+		if ans == "C" || ans == "c" {
+			fmt.Println("Good job!")
+		} else {
+			fmt.Println(ans, "isn't correct. Try again!")
+		}
+	case num == 3:
+		var ans string
+		fmt.Println("When was Robinson Crusoe, by Daniel Defoe, published?")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+
+		if ans == "1719" {
+			fmt.Println("That's right!")
+		} else {
+			fmt.Println(ans, "isn't correct. Try again!")
+		}
+	case num == 4:
+		var ans string
+		fmt.Println("Who wrote Beowulf?")
+		fmt.Scanln(&ans)
+		ans = string(ans)
+
+		if ans == "anonymous" {
+			fmt.Println("That's right, we don't know who wrote this poem!")
+		} else {
+			fmt.Println(ans, "isn't correct. Try again!")
+		}
+	}
+}
+
+func costiCuriosity(num int) {
+	switch {
+	case num == 1:
+		fmt.Println("In April 1891, Oscar Wilde's first novel The Picture of Dorian Gray was published as a book.")
+	case num == 2:
+		fmt.Println("Gulliver plays the role of a benevolent giant for little people who have exaggerated ideas about their self-importance.")
+	case num == 3:
+		fmt.Println("Robinson Crusoe is a novel by Daniel Defoe, first published on 25 April 1719.")
+	case num == 4:
+		fmt.Println("Bwowulf is an anonymous epic poem, written in a West Saxon variant of Anglo-Saxon")
+	}
+}
+
 func main() {
-	assessment()
+	questions := [4]int{1, 2, 3, 4}
+	quest := questions[rand.Intn(len(questions))]
 	app := &cli.App{
 		Commands: []*cli.Command{
 			{
@@ -28,17 +155,16 @@ func main() {
 						Name:  "test",
 						Usage: "gives you a question",
 						Action: func(cCtx *cli.Context) error {
-
-							var ans int
-							fmt.Println("How many bits are contained in a byte?")
-							fmt.Scanln(&ans)
-							ans = int(ans)
-
-							if ans == 8 {
-								fmt.Println("In a byte there are 8 bits ")
-							} else {
-								fmt.Println("In a byte there aren't", ans, "bits. Try again!")
-							}
+							assessment()
+							cociTest(quest)
+							return nil
+						},
+					},
+					{
+						Name:  "curiosity",
+						Usage: "gives you explantion on topic",
+						Action: func(cCtx *cli.Context) error {
+							cociCuriosity(quest)
 							return nil
 						},
 					},
@@ -53,16 +179,16 @@ func main() {
 						Name:  "test",
 						Usage: "gives you a question",
 						Action: func(cCtx *cli.Context) error {
-							var ans string
-							fmt.Println("Who wrote Dorian Gray?")
-							fmt.Scanln(&ans)
-							ans = string(ans)
-
-							if ans == "Oscar Wilde" {
-								fmt.Println("Dorian Gray was written by Oscar Wilde")
-							} else {
-								fmt.Println("Dorian Gray wasn't written by ", ans, "Try again!")
-							}
+							assessment()
+							costiTest(quest)
+							return nil
+						},
+					},
+					{
+						Name:  "curiosity",
+						Usage: "gives you explantion on topic",
+						Action: func(cCtx *cli.Context) error {
+							costiCuriosity(quest)
 							return nil
 						},
 					},
